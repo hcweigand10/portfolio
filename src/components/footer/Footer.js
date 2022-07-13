f00f9d9477d9b7751f48c85b13a73daa86657938
hcweigand10 <henryweigand10@gmail.com>
2022-07-13 14:55:11 -0700

    warnings

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { faEnvelope, faHome } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "./footer.css"
